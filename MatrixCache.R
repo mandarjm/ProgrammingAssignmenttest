6c5a93b69a979bb0c5762fdfc8283d449f6b0118
mandarjm <mandar.morekar@gmail.com>
2015-08-22 19:47:05 -0700

    Added Comments
    Added verbose comments to cachesovle function and modified comments in
    makeCacheMatrix function

--- MatrixCache.R
+++ MatrixCache.R
@@ -1,30 +1,35 @@
-#defining function makeCacheMatrix to accept matrix x as input 
+## defining function makeCacheMatrix to accept matrix x as input 
 makeCacheMatrix <- function(x = matrix()) 
 { 
-  #setting up variable to store inverse matrix
+  ## setting up variable to store inverse matrix
   v_inv <- NULL 
-  #function to set value of matrix  
+  ## function to set value of matrix  
   set <- function(y) 
   { 
+    ## assigning value to cached environment
     x <<- y 
     v_inv <<- NULL 
   } 
-  #function to get value of matrix    
+  ## function to get value of matrix    
   get <- function() x 
-  #function to set value of inverse of matrix
+  ## function to set value of inverse of matrix
   setinverse <- function(inverse) v_inv <<- inverse 
-  #function to get value of inverse of matric
+  ## function to get value of inverse of matric
   getinverse <- function() v_inv 
   list(set=set, get=get, setinverse=setinverse, getinverse=getinverse) 
 } 
 
+## Computes the inverse. If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cach 
 
 cacheSolve <- function(x, ...) { 
   v_inv <- x$getinverse() 
+  ##check is matrix inverse is present in cache, if yes return.
   if(!is.null(v_inv)) { 
     message("Retrieving data from cache.") 
     return(v_inv) 
   } 
+  ##if matrix inverse is not calculated the calculate it and set it in cached environment
   data <- x$get() 
   v_inv <- solve(data) 
   x$setinverse(v_inv) 
